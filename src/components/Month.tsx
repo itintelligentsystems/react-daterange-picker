89577f338c5663426e8dc3f13c4382610ad527bf
Reinis <reinis.upitis@intelligentsystems.lv>
2020-07-01 16:28:26 +0300

    make package.json relevant for this fork, week starts with monday now

--- src/components/Month.tsx
+++ src/components/Month.tsx
@@ -21,7 +21,7 @@ import Header from "./Header";
 import Day from "./Day";
 import { NavigationAction, DateRange } from "../types";
 
-const WEEK_DAYS = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
+const WEEK_DAYS = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
 
 const styles = (_: Theme) =>
 	createStyles({
